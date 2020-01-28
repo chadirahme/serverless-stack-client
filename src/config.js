4228d4f4f2cd976ce8dc4f33f9df3a540e6cf009
chadirahme <eng.chadi@gmail.com>
2020-01-28 15:17:30 -0500

    Add Login with Cognito AWS and new Routes

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "appointment-passion-app-bucket"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://oubssho707.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_ihQ1V0Nej",
+        APP_CLIENT_ID: "1j1t391t560gcsd8onrn53kigm",
+        IDENTITY_POOL_ID: "us-east-1:fc69169d-b89a-47d1-b13b-09b6a5aed403"
+    }
+};
